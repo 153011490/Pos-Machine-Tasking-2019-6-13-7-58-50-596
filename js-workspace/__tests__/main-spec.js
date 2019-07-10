a9377441b0d34eaf1b733369161f352b69db7f8e
SamsonLin <153011490@qq.com>
2019-07-10 10:48:56 +0800

    format main.js

--- js-workspace/__tests__/main-spec.js
+++ js-workspace/__tests__/main-spec.js
@@ -2,9 +2,9 @@ const printReceipt = require('../main');
 it ("should return receipt when call printReceipt given ['0001','0003', '0005', '0003']", () => {
   expect(printReceipt(['0001','0003', '0005', '0003'])).toBe('Receipts\n' +
       '------------------------------------------------------------\n' +
-      'Coca Cola  3  1\n' +
-      'Pepsi-Cola  5  2\n' +
-      'Dr Pepper  7  1\n' +
+      'Coca Cola\t3\t1\n' +
+      'Pepsi-Cola\t5\t2\n' +
+      'Dr Pepper\t7\t1\n' +
       '------------------------------------------------------------\n' +
       'Price: 20');
 });
--- js-workspace/main.js
+++ js-workspace/main.js
@@ -1,64 +1,65 @@
-function isBarCodeValid(barCode){
+const isBarCodeValid = (barCode) =>{
     if(typeof(getItemById(barCode).id)=="undefined"){
         return false;
     }
     return true;
 }
 
-function getItemById(id) {
+const getItemById = (id) =>{
     let itemsDB = [
-      {"id": "0001", "name": "Coca Cola", "price": 3},
-      {"id": "0002", "name": "Diet Coke", "price": 4},
-      {"id": "0003", "name": "Pepsi-Cola", "price": 5},
-      {"id": "0004", "name": "Mountain Dew", "price": 6},
-      {"id": "0005", "name": "Dr Pepper", "price": 7},
-      {"id": "0006", "name": "Sprite", "price": 8},
-      {"id": "0007", "name": "Diet Pepsi", "price": 9},
-      {"id": "0008", "name": "Diet Mountain Dew", "price": 10},
-      {"id": "0009", "name": "Diet Dr Pepper", "price": 11},
-      {"id": "0010", "name": "Fanta", "price": 12}
-    ];
-    let item={};
-    for(let index=0;index<itemsDB.length;index++){
-        if(id==itemsDB[index].id){
-            item=itemsDB[index];
-            return item;
-        }
-    }
-    return item;
-  }
+        {"id": "0001", "name": "Coca Cola", "price": 3},
+        {"id": "0002", "name": "Diet Coke", "price": 4},
+        {"id": "0003", "name": "Pepsi-Cola", "price": 5},
+        {"id": "0004", "name": "Mountain Dew", "price": 6},
+        {"id": "0005", "name": "Dr Pepper", "price": 7},
+        {"id": "0006", "name": "Sprite", "price": 8},
+        {"id": "0007", "name": "Diet Pepsi", "price": 9},
+        {"id": "0008", "name": "Diet Mountain Dew", "price": 10},
+        {"id": "0009", "name": "Diet Dr Pepper", "price": 11},
+        {"id": "0010", "name": "Fanta", "price": 12}
+      ];
+      let item={};
+      for(let index=0;index<itemsDB.length;index++){
+          if(id==itemsDB[index].id){
+              item=itemsDB[index];
+              return item;
+          }
+      }
+      return item;
+}
 
-function convertToItemList(barCodes) {
+const convertToItemList = (barCodes) => {
     let itemList = [];
     barCodes=barCodes.sort();
+    let count=0;
     for(let i=0;i<barCodes.length;i+=count){
-        var count=0;
+        count=0;
         if(isBarCodeValid(barCodes[i])){
             for (let j = i; j < barCodes.length; j++) {  
                 if (barCodes[i] == barCodes[j]) {  
                     count++;  
                 }  
             }  
-            itemList.push({"ID":barCodes[i],"count":count});  
+            itemList.push({"ID":barCodes[i],count});  
         }else{
             return "[ERROR]: barcode is not found";
         }
     }
     return itemList;
-  }
-  
-  function getReceiptByItemList(itemList) {
+}
+
+const getReceiptByItemList = (itemList) => {
     let total = 0;
     let receipt = '';
-    receipt += 'Receipts\n' +'------------------------------------------------------------\n';
+    receipt += 'Receipts\n------------------------------------------------------------\n';
     for(let i=0;i<itemList.length;i++){
         let item=getItemById(itemList[i].ID);
-        receipt += item['name'] + '  ' + item['price'] + '  ' + itemList[i].count + '\n';
+        receipt += `${item.name}\t${item.price}\t${itemList[i].count}\n`;
         total += item['price']*itemList[i].count;
     }
     receipt += ('------------------------------------------------------------\nPrice: ' + total);
     return receipt;
-  }
+}
   
   function printReceipt(barcodeList) {
     let itemList = convertToItemList(barcodeList);
